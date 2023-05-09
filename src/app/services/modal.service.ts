f45d2c7d89867af1244bf96f38665d23d4899f17
lebocow <tzmic1@gmail.com>
2023-05-09 20:41:57 +0300

    Created modal, nav, auth-modal components and also modal service

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: Map<string, IModal> = new Map();
+
+  isModalOpen(id: string): boolean {
+    return this.modals.get(id)?.visible ?? false;
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.get(id);
+    modal && (modal.visible = !modal.visible);
+  }
+
+  register(id: string) {
+    this.modals.set(id, {
+      id,
+      visible: false,
+    });
+  }
+
+  unregister(id: string) {
+    this.modals.delete(id);
+  }
+}
